ec72c6c23da0acc2fcdd22edaef785ac40cc9f70
bikashsahu28 <bikashsahu3046@gmail.com>
2025-07-04 18:02:46 +0530

    add data

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './connectdb.js';
-// import cors from 'cors';
+import cors from 'cors';
 import router from './routes/routes.js';
+import cookieParser from 'cookie-parser';
 const app = express();
 dotenv.config();
-// app.use(cors());
+app.use(cors({
+  origin:'http://localhost:5173',
+}));
+app.use(cookieParser());
 app.use(express.json());
 app.use('/api', router);
 connectDB();
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import {loanApplyController} from '../controller/loan.controller.js';
 const router = express.Router();
-router.route('/getloan').post(loanApplyController);
+// router.route('/getloan').post(loanApplyController);
+router.route('/postloan').post(loanApplyController);
 export default router;
